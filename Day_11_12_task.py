1d38094956872ac1a4b8d5bc8f42cbd2c88f6c95
macbook <macbook@macbooks-MacBook-Pro.local>
2020-06-07 15:01:25 -0500

    updated task 3

--- Day_11_12_task.py
+++ Day_11_12_task.py
@@ -348,6 +348,96 @@ while counter<=5:
 
 
 
+#TASK 3
+
+#1 Create a list of the 10 elements of four different types of Data Type like int, string, complex and float.
+
+x=[23,"ConsultAdd",45,87.5,"bootcamp",2+3j,55,90,"python",6.7]
+print(type(x))
+
+#2 Create a list of size 5 and execute the slicing structure 
+
+x=[33,67,87,56,64]
+x[1:4]
+
+#3 Write a program to get the sum and multiply of all the items in a given list.
+
+x=[2,4,6,8,10]
+Sum_of_x=sum(x) 
+print(Sum_of_x)
+
+for i in x:
+    i*=i
+print(i)
+
+#4 Find the largest and smallest number from a given list.
+
+x=[2,3,55,76,45,98]
+print(max(x))
+print(min(x))
+
+#5 Create a new list which contains the specified numbers after removing the even numbers from a predefined list.
+x=[2,3,5,6,7,8,45,34,87,55]
+for i in x:
+    if(i%2==0):
+        x.remove(i)
+print(x)
+
+#6 Create a list of first and last 5 elements where the values are square of numbers between 1 and 30 (both included).
+
+x=[]
+
+for i in range(1,31):
+    x.append(i**2)
+
+print(x[:5])
+
+#7 Write a program to replace the last element in a list with another list.
+    #Sample data: [[1,3,5,7,9,10],[2,4,6,8]]
+    #Expected output: [1,3,5,7,9,2,4,6,8]
+
+x=[1,3,5,7,9,10]
+y=[2,4,6,8]
+x[-1:] = y
+print(x)
+
+
+#8 Create a new dictionary by concatenating the following two dictionaries:
+    #a={1:10,2:20}
+    #b={3:30,4:40}
+    #Expected Result: {1:10,2:20,3:30,4:40}
+
+a={1:10,2:20}
+b={3:30,4:40}
+c={}
+c.update(a)
+c.update(b)
+print(c)
+
+#9 Create a dictionary that contains a number (between 1 and n) in the form(x,x*x).
+    #Sample data (n=5)
+    #Expected Output: {1:1,2:4,3:9,4:16,5:25}
+
+n={}
+for i in range(1,6):
+    n[i]=i*i
+print(n)
+
+
+#10 Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number. Suppose the following input is supplied to the program:
+    #34,67,55,33,12,98
+    #The output should be:
+    #[‘34’,’67’,’55’,’33’,’12’,’98’]
+    #(‘34’,’67’,’55’,’33’,’12’,’98’)
+
+x = input("Please enter nos. separated by commas : ")
+list = x.split(",")
+tuple = tuple(list)
+print('List : ',list)
+print('Tuple : ',tuple)
+
+
+
 
 
 
